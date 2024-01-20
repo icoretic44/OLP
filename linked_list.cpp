720bac00b2e7dabc6268c5ad6dfd24694228d638
icoretic44 <77340716+icoretic44@users.noreply.github.com>
2024-01-21 00:17:24 +0700

    add Linked list

--- linked_list.cpp
+++ linked_list.cpp
@@ -35,6 +35,23 @@ void inSert_last(Linked &fi,Linked &la,int k)
     la = new_node; // gan la(moi) la new_node
     return;
 }
+Linked find_pos(Linked fi,int pos)
+{
+    int cnt = 1;
+    while(cnt<pos-1)
+    {
+        ++cnt;
+        fi = fi->next;
+    }
+    return fi;
+}
+void inSert_anywhere(Linked &point_,int k)
+{
+    Linked new_node = Create_Node(k);
+    new_node->next = point_->next;
+    point_->next = new_node;
+    return;
+}
 void printMonitor(Linked NodE)
 {
     while(NodE != NULL)
@@ -42,6 +59,13 @@ void printMonitor(Linked NodE)
         printf("%d ",NodE->value);
         NodE = NodE->next;
     }
+    return;
+}
+void delete_anywhere(Linked &point_)
+{
+    Linked tmp = point_->next;
+    point_->next = tmp->next;
+    free(tmp);
     return;
 }
 int main()
@@ -56,8 +80,17 @@ int main()
         scanf("%d",&k);
         inSert_last(fi,la,k);
     }
+    //insert anywhere
+    int pos,k;
+    scanf("%d%d",&pos,&k);
+    Linked point_ = find_pos(fi,pos);
+    inSert_anywhere(point_,k);
     Linked a = fi;
     printMonitor(a);
+    //delete anywhere
+    scanf("%d",&pos);
+    point_ = find_pos(fi,pos);
+    delete_anywhere(point_);
     printf("\n");
     printMonitor(a);
     return 0;
